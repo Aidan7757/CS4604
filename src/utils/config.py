74cf9d7bdb7b30d84fdaecc4c600b9afa85f9b75
Aidan Chadha <aidanchadha03@vt.edu>
2025-10-29 11:44:40 -0400

    Delete src directory

--- src/utils/config.py
+++
@@ -1,9 +0,0 @@
-# Database configuration
-import os 
-
-DB_CONFIG = {
-    'user': os.getenv('DB_USER', 'root'),
-    'password': os.getenv('DB_PASSWORD', ''),
-    'host': os.getenv('DB_HOST', '127.0.0.1'),
-    'database': os.getenv('DB_NAME', 'CS4604')
-}
