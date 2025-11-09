78630e24a8d345c675c689f1ff242ebd479aacb7
Emily Vu <emily.vu@sapns2.com>
2025-11-09 17:04:56 -0500

    add basic home page and nav bar

---
+++ ui/src/components/HomePage.js
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import './HomePage.css';
+
+export default function HomePage() {
+  return (
+    <div className="home-page">
+      <h1 className="title">Explore</h1>
+      <div className="card-container">
+        <Link to="/alerts" className="card">
+          <h2>Alerts</h2>
+          <p>View alerts for all parks</p>
+        </Link>
+        <Link to="/parks" className="card">
+          <h2>Parks</h2>
+          <p>Explore all the national parks</p>
+        </Link>
+        <Link to="/species" className="card">
+          <h2>Species</h2>
+          <p>View information about species</p>
+        </Link>
+        <Link to="/visitors" className="card">
+          <h2>Visitors</h2>
+          <p>See visitor data</p>
+        </Link>
+        <Link to="/organizations" className="card">
+          <h2>Organizations</h2>
+          <p>View organization information</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
